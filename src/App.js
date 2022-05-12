e3c1c4e3dc1828942607fad61b64464a3de6ce68
Adan <adanaligakhar@gmail.com>
2022-05-12 19:06:00 +0500

    setup files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import { Navbar, Sidebar, Footer } from './components';
+import styled from 'styled-components';
+
+const Button = styled.button `
+background : green;
+color : white;
+
+
+`
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return <div>
+    <h4></h4>
+    <Button>click me</Button>
+  </div>
 }
 
 export default App
