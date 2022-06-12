7f981291249b2bad688fb7a36705db02d7b95e53
Adan <adanaligakhar@gmail.com>
2022-06-12 18:00:51 +0500

    worked on sorting and filtering

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -17,6 +17,18 @@ const initialState = {
   filtered_products : [],
   all_products : [],
   grid_view : true,
+  sort : 'price-lowest',
+  filters : {
+    text : '',
+    company : 'all',
+    category : 'all',
+    color : 'all',
+    min_price : 0,
+    max_price : 0,
+    actual_price : 0,
+    shipping : false,
+
+  }
 
 }
 
@@ -28,12 +40,35 @@ export const FilterProvider = ({ children }) => {
   const [state,dispatch] = useReducer(reducer,initialState);
 
   useEffect(() => {
+
+    dispatch({type : SORT_PRODUCTS})
+
+
+  },[products,state.sort])
+
+  useEffect(() => {
     dispatch({type : LOAD_PRODUCTS, payload : products})
 
   },[products])
-  
+
+  const setGridView = () => {
+    dispatch({type : SET_GRIDVIEW})
+
+  }
+  const setListView = () => {
+    dispatch({type : SET_LISTVIEW})
+
+  }
+
+
+  const updateSort = (e) => {
+    // const name = e.target.name;
+    const value = e.target.value;
+    dispatch({type : UPDATE_SORT, payload : value})
+
+  }
   return (
-    <FilterContext.Provider value={{...state}}>
+    <FilterContext.Provider value={{...state,setGridView,setListView,updateSort}}>
       {children}
     </FilterContext.Provider>
   )
